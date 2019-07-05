39acc200e14862c165b1c5ddec0107835ab1c6a7
Ikkimaru <ebenvrensburg@hotmail.com>
2019-07-05 13:08:42 +0200

    Changed to search movie folders in current directory and added a check to operating system

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os	#List Directories
+import platform		#Identify OS (Similar to 'os' above)
 import re 	#Split String
 from google_images_download import google_images_download   #Importing Image Search Library
 from PIL import Image 	#Conver To Ico
 
 
-path = '/home/eben/Python/MovieIconsForFolders/Movies'
+path = os.getcwd()
 
 folders = []
 
@@ -24,13 +25,24 @@ for item in os.listdir(path):
 response = google_images_download.googleimagesdownload()   #class instantiation
 
 for file in folders:
-	arguments = {"keywords": file + " Movie Poster","limit":1,"print_urls":True, "output_directory": path, "image_directory": file}   #creating list of arguments
+	arguments = {"keywords": file + " Movie Poster","limit":1,"print_urls":True, "output_directory": path, "image_directory": file}   # "format": 'ico's
 	paths = response.download(arguments)   #passing the arguments to the function
 	print(paths)   #printing absolute paths of the downloaded images
 
-
-#Code From: 
-#https://github.com/python-pillow/Pillow/issues/1102#issuecomment-73298481
-
+#Change images to ICO
 #original = Image.open('/home/eben/Python/MovieIconsForFolders/Movies/Avatar/1.61OUGpUfAyL._SY679_.jpg') # you can try with whatever format
 #original.save('/home/eben/Python/MovieIconsForFolders/Movies/Avatar/newfavicon.ico') # not recognized on XP
+
+#Set icons based on OS
+def linuxIcons():
+    print ("Linux Wins.\n")
+
+def windowsIcons():
+    print ("Windows Wins.\n")
+
+
+options = {'Linux' : linuxIcons,
+           'Windows' : windowsIcons,
+}
+
+options[platform.system()]() #Identify OS and run code based on result
